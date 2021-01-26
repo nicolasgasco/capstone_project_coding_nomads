726b523230b06f1c58288958401addbf7cf9a4de
nicolasgasco <nicolas.gasco92@gmail.com>
2021-01-26 16:31:25 +0100

    Optimized and refactored two main scripts

---
+++ get_tweets.py
@@ -0,0 +1,121 @@
+import os
+import tweepy
+import sqlalchemy
+from pprint import pprint
+import datetime
+import json
+
+# In this file, user_id in db are used to fetch last 20 tweets (English and no RT)
+
+# fetch the secrets from our virtual environment variables
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+
+
+# authenticate to the service we're accessing
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+
+# Password stored in another file
+file = r"C:\Users\nicol\Dropbox\Coding\password_SQL.txt"
+with open(file) as f:
+    password = f.read()
+    password = password.replace("\"", "").strip()
+
+
+# Connecting to database
+engine = sqlalchemy.create_engine(f"mysql+pymysql://root:{password}@localhost/tweetsdb")
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+
+# Using both tweets and users tables
+table_users = sqlalchemy.Table("users", metadata, autoload=True, autoload_with=engine)
+table_tweets = sqlalchemy.Table("tweets", metadata, autoload=True, autoload_with=engine)
+
+# Fetch from user_id column in users table
+query = sqlalchemy.select([table_users.columns.user_id])
+result_proxy = connection.execute(query)
+
+result_set = result_proxy.fetchall()
+
+# File containing user_id which were already searched
+# Script can be run several times without duplicating results or wasting time on already researched users
+file_for_already_searched_users = "users_searched_already.txt"
+
+
+# Make a list out of IDs in the file
+with open(file_for_already_searched_users) as file:
+    list_already_searched_users = file.readlines()
+
+# Keep truck of users searched
+user_number = 1
+# user is a tuple, only index 0 is relevant
+for user in result_set:
+    # If user was searched already
+    if f"{user[0]}\n" in list_already_searched_users:
+        print(f"User {user_number} was skipped.")
+        user_number += 1
+        continue
+
+    # Connection created only for new IDs
+    else:
+        # create the connection
+        api = tweepy.API(auth)
+        tweets = api.user_timeline(user[0])
+
+        # This is to avoid error like max limit reach
+        try:
+            # Keeping track of all tweets and those successfully added to database
+            number_tweets_total = 1
+            number_tweets_successful = 1
+
+            for tweet in tweets:
+                # Status needed to access status info (one layer beyond tweet)
+                status = api.get_status(tweet.id, tweet_mode="extended")
+                data_for_tweets = {
+                    "user_id": tweet.user.id,
+                    "tweet_id": tweet.id,
+                    "created_at": tweet.created_at,
+                    "full_text": status.full_text,
+                    "screen_name": tweet.user.screen_name,
+                }
+
+                # Insert in table tweets
+                query_tweets = sqlalchemy.insert(table_tweets)
+
+                # We don't want retweets nor tweets in languages other than English
+                if data_for_tweets["full_text"][:2] != "RT" and status.lang == "en":
+
+                    # This is to avoid error caused by duplicates, only unique users wanted
+                    try:
+
+                        result_proxy = connection.execute(query_tweets, data_for_tweets)
+                        print(f"User number {user_number} ({user[0]}). Tweet {number_tweets_successful} of {number_tweets_total} done.")
+
+                        number_tweets_successful += 1
+                        number_tweets_total += 1
+
+                    # Exception mostly raised for duplicates
+                    except Exception as e:
+                        print(f"User number {user_number}. Tweet {number_tweets_total} was skipped (duplicate).")
+                        number_tweets_total += 1
+                        continue
+
+                # Tweets which are RTs, in language other than English or both
+                else:
+                    print(f"User number {user_number}. Tweet {number_tweets_total} was skipped (RT or language other than English).")
+                    number_tweets_total += 1
+
+            user_number += 1
+
+            # Placed here so that if not all 20 tweets from a user were fetched, user isn't counted
+            with open(file_for_already_searched_users, "a") as file:
+                file.write(f"{user[0]}\n")
+
+        # This is mostly for limit reached
+        except Exception as e:
+            print(f"The script ended because of an error ({e}). Please, try again.")
+            break
---
+++ get_users_list.py
@@ -0,0 +1,97 @@
+import os
+import tweepy
+import sqlalchemy
+import datetime
+from pprint import pprint
+
+# In this file, user_id are fetched filtering tweets written in English and containing the word "cyberpunk"
+
+# fetch the secrets from virtual environment variables
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+
+
+# authenticate to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+
+# What I'm looking for and how many results
+limit_search = 5
+api = tweepy.API(auth)
+tweets = tweepy.Cursor(api.search,
+q="cyberpunk",
+lang="en",
+).items(limit_search)
+
+
+# Password stored in another file for safety
+file = r"C:\Users\nicol\Dropbox\Coding\password_SQL.txt"
+with open(file) as f:
+    password = f.read()
+    password = password.replace("\"", "").strip()
+
+
+# Connecting to database used for project
+engine = sqlalchemy.create_engine(f"mysql+pymysql://root:{password}@localhost/tweetsdb")
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+
+
+# Store the users who talked about Cyberpunk recently in table 'users'
+table_users = sqlalchemy.Table("users", metadata, autoload=True, autoload_with=engine)
+
+
+# This is to avoid errors like max limit reach more gracefully
+try:
+    # Counting error to stop if there are too many
+    err_count = 0
+    # Counting total tweets and those which meet criteria
+    number_tweets_total = 1
+    number_tweets_successful = 1
+
+    # for every tweet found in the query
+    for tweet in tweets:
+
+        # We only care about users and user stats, no text
+        # Dictionary matching db table pattern
+        data_for_users = {
+            "user_id": tweet.user.id,
+            "followers_count": tweet.user.followers_count,
+            "friends_count": tweet.user.friends_count,
+            "screen_name": tweet.user.screen_name,
+            "statuses_count": tweet.user.statuses_count,
+        }
+
+        # Insert in db table
+        query_users = sqlalchemy.insert(table_users)
+
+        # This is to avoid error caused by duplicates, only unique users wanted
+        try:
+            result_proxy = connection.execute(query_users, data_for_users)
+            print(f"Entry number {number_tweets_successful} of {number_tweets_total} done.")
+            number_tweets_successful += 1
+            number_tweets_total += 1
+
+        except Exception as e:
+            print(f"Number {number_tweets_total} was skipped ({e}).")
+            number_tweets_total += 1
+
+            # Break after 10 non consecutive errors
+            if err_count >= 10:
+                break
+            err_count += 1
+            continue
+
+    # Either ends on its own or because of an error
+    if err_count < 10:
+        print("\nThe search is complete")
+    else:
+        print("\nThe script was interrupted. Too many errors.")
+
+# This is mostly max limit reached error
+except Exception as e:
+    print(f"The script ended because of an error ({e}). Please, try again.")
+
