c2d701442eab9417a58712536c1e87605006524d
nicolasgasco <nicolas.gasco92@gmail.com>
2021-01-28 15:25:16 +0100

    Trimmed file and added function to write results to txt file

--- tweets_user_analysis.py
+++ tweets_user_analysis.py
@@ -1,17 +1,15 @@
 import os
 import sqlalchemy
 from tweets_users_functions import *
-import re
-from random import randint
+from datetime import datetime
 
-# This is where the actual data analysis will take place, using the data in the database
+# In this file, user data is analyzed
 
-# Password stored in another file for security
+# Password stored in another file for safety
 file = r"C:\Users\nicol\Dropbox\Coding\password_SQL.txt"
 with open(file) as f:
     password = f.read()
     password = password.replace("\"", "").strip()
-
 
 # Connecting to database
 engine = sqlalchemy.create_engine(f"mysql+pymysql://root:{password}@localhost/tweetsdb")
@@ -19,11 +17,11 @@ connection = engine.connect()
 metadata = sqlalchemy.MetaData()
 
 
-# Using both tweets and users tables
+# Using both 'tweets' and users 'tables'
 table_users = sqlalchemy.Table("users", metadata, autoload=True, autoload_with=engine)
 table_tweets = sqlalchemy.Table("tweets", metadata, autoload=True, autoload_with=engine)
 
-# Fetch from user_id column in users table
+# Select both tables
 query_users = sqlalchemy.select([table_users])
 query_tweets = sqlalchemy.select([table_tweets])
 
@@ -33,90 +31,126 @@ result_proxy_tweets = connection.execute(query_tweets)
 result_set_users = result_proxy_users.fetchall()
 result_set_tweets = result_proxy_tweets.fetchall()
 
-print(f"The following stats were obtained by analyzing {len(result_set_tweets)} tweets from {len(result_set_users)} users.\n")
+# First a general overview of the data
+line1 = f"The following stats were obtained by analyzing {len(result_set_tweets)} tweets from {len(result_set_users)} users.\n"
+print(line1)
 
-print("User-related stats:")
+line2 = "User-related stats:\n"
+print(line2)
+
+# Average values
 # The average number of followers.
 average_followers_count = average_followers(result_set_users)
-print(f"The average number of followers is {int(average_followers_count)}.")
-
+line3 = f"The average number of followers is {int(average_followers_count)}."
+print(line3)
 
 # The average number of tweets published by users.
 average_status_count = average_status_count(result_set_users)
-print(f"The average number of statuses is {int(average_status_count)}.")
+line4 = f"The average number of statuses is {int(average_status_count)}."
+print(line4)
 
 # The average number of friends
 average_followed_count = average_followed(result_set_users)
-print(f"The average number of friends (followed accounts) is {int(average_followed_count)}.")
+line5 = f"The average number of friends (followed accounts) is {int(average_followed_count)}."
+print(line5)
 
 # The average followers/friends ratio
 average_followers_friends_ratio = find_average_followers_friends_ratio(result_set_users)
-print(f"The average followers/friends ratio is {int(average_followers_friends_ratio)} followers for every followed account.")
+line6 = f"The average followers/friends ratio is {int(average_followers_friends_ratio)} followers for every followed account."
+print(line6)
 
 
+# Highest values
 print("\n")
 
 # Highest number of tweets
 highest_status = find_most_statuses(result_set_users)
-print(f"The user with the highest status count is {highest_status[1]} with {highest_status[0]} statuses.")
+line7 = f"The user with the highest status count is {highest_status[1]} with {highest_status[0]} statuses."
+print(line7)
 
 # Highest number of followers
 highest_followers = find_highest_followers(result_set_users)
-print(f"The user with the highest followers count is {highest_followers[1]} with {highest_followers[0]} followers.")
-
+line8 = f"The user with the highest followers count is {highest_followers[1]} with {highest_followers[0]} followers."
+print(line8)
 
 # Highest number of friends
 highest_friends = find_highest_friends(result_set_users)
-print(f"The user with the highest friend count is {highest_friends[1]} with {highest_friends[0]} friends.")
+line9 = f"The user with the highest friend count is {highest_friends[1]} with {highest_friends[0]} friends."
+print(line9)
 
 # User with the highest followers/friends ratio
 user_highest_ratio = find_highest_followers_friends_ratio(result_set_users)
-print(f"The user with highest followers/friends ratio ({int(user_highest_ratio[1])} followers for every friend) is {user_highest_ratio[0]} with {user_highest_ratio[2][0]} followers and {user_highest_ratio[2][1]} friends.")
-# # What user has the least statuses in the dataset?
-# lowest_status = find_least_statuses(result_set_users)
-# print(f"The user with the lowest status count is {lowest_status[1]} with {lowest_status[0]} status(es).")
+line10 = f"The user with highest followers/friends ratio ({int(user_highest_ratio[1])} followers for every friend) is {user_highest_ratio[0]} with {user_highest_ratio[2][0]} followers and {user_highest_ratio[2][1]} friends."
+print(line10)
+
 
 print("\n")
+# Follower counts
 
-# Print 5 users without followers
+# 5 users without followers
 user_without_followers = find_users_without_followers(result_set_users)
 limit = user_without_followers[1]
 users_list = user_without_followers[0]
+# This is to add Oxford comma and 'and' before final item
 users_list_formatted = f"{(', '.join(users_list[:limit-1]))}, and {str(user_without_followers[0][-1])}"
-print(f"These are {user_without_followers[1]} (or less) users who don't have any followers at all: {users_list_formatted}.")
+line11 = f"These are {user_without_followers[1]} (or less) users who don't have any followers at all: {users_list_formatted}."
+print(line11)
 
 # X users with highest followers count (X = 5 per default, but can be changed)
 users_highest_followers = find_users_most_followers(result_set_users)
 limit = users_highest_followers[1]
 users_list = users_highest_followers[0]
+# This is to add Oxford comma and 'and' before final item
 users_list_formatted = f"{(', '.join(users_list[:limit-1]))}, and {str(users_list[-1])}"
-print(f"The {users_highest_followers[1]} users with the highest follower counts are {users_list_formatted}.")
+line12 = f"The {users_highest_followers[1]} users with the highest follower counts are {users_list_formatted}."
+print(line12)
 
 # Number of users with less than x followers, but y statuses. x, y can be passed as parameters, eg. (dataset, limit_followers=50, limit_statuses=500)
 users_no_followers_but_statuses = find_users_without_followers_but_statuses(result_set_users)
-print(f"There are {users_no_followers_but_statuses[0]} users with less than {users_no_followers_but_statuses[2]} followers, but at least {users_no_followers_but_statuses[1]} statuses.")
+line13 = f"There are {users_no_followers_but_statuses[0]} users with less than {users_no_followers_but_statuses[2]} followers, but at least {users_no_followers_but_statuses[1]} statuses."
+print(line13)
 
 users_no_followers_but_statuses = find_users_without_followers_but_statuses(result_set_users, 100, 500)
-print(f"There are {users_no_followers_but_statuses[0]} users with less than {users_no_followers_but_statuses[2]} followers, but at least {users_no_followers_but_statuses[1]} statuses.")
+line14 = f"There are {users_no_followers_but_statuses[0]} users with less than {users_no_followers_but_statuses[2]} followers, but at least {users_no_followers_but_statuses[1]} statuses."
+print(line14)
 
 print("\n")
-
-# Percentage of users with less than x followers (default is 100, can also be passed as parameter
-user_with_less_followers = percentage_user_with_less_x_followers(result_set_users, 100)
-print(f"The percentage of users with less than {user_with_less_followers[1]} followers is {int(user_with_less_followers[0])}%.")
-
-user_with_less_followers = percentage_user_with_less_x_followers(result_set_users, 500)
-print(f"The percentage of users with less than {user_with_less_followers[1]} followers is {int(user_with_less_followers[0])}%.")
-
+# Users with few friends
 # Average of followers of users with less than x friends (default is 5, can be changed)
 average_users_no_friends = average_followers_users_no_friends(result_set_users)
-print(f"On average, users who follow less than {average_users_no_friends[1]} other users have {int(average_users_no_friends[0])} followers.")
+line15 = f"On average, users who follow less than {average_users_no_friends[1]} other users have {int(average_users_no_friends[0])} followers."
+print(line15)
 
 average_users_no_friends = average_followers_users_no_friends(result_set_users, 100)
-print(f"On average, users who follow less than {average_users_no_friends[1]} other users have {int(average_users_no_friends[0])} followers.")
+line16 = f"On average, users who follow less than {average_users_no_friends[1]} other users have {int(average_users_no_friends[0])} followers."
+print(line16)
 
+
+print("\n")
+# Percentages of users with few followers
+# Percentage of users with less than x followers (default is 100, can also be passed as parameter
+user_with_less_followers = percentage_user_with_less_x_followers(result_set_users, 100)
+line17 = f"The percentage of users with less than {user_with_less_followers[1]} followers is {int(user_with_less_followers[0])}%."
+print(line17)
+
+user_with_less_followers = percentage_user_with_less_x_followers(result_set_users, 500)
+line18 = f"The percentage of users with less than {user_with_less_followers[1]} followers is {int(user_with_less_followers[0])}%."
+print(line18)
+
+print("\n")
+# ID stats
 # Accounts with the highest and smallest IDs
 smallest_highest_id = find_smallest_highest_id(result_set_users)
 smallest_id = smallest_highest_id[0]
 highest_id = smallest_highest_id[1]
 print(f"The user with the smallest ID ({smallest_id[1]}) is {smallest_id[0]}, while the biggest ID ({highest_id[1]}) belongs to {highest_id[0]}.")
+
+file = "results_users_analysis.txt"
+now = datetime.now()
+lines = [line1, line2, line3, line4, line5, line6, "\n", line7, line8, line9, line10, "\n", line11, line12, line13,
+         line14, "\n", line15, line16, "\n", line17, line18]
+with open(file,'w') as f:
+    f.write(f"These results were generated on {now.strftime('%B, %d %Y at %H:%M:%S')} (GMT+1)." + "\n\n\n")
+
+    for line in lines:
+        f.writelines(f"{line}\n")
--- tweets_users_functions.py
+++ tweets_users_functions.py
@@ -1,87 +1,110 @@
 import os
 import re
-from random import randint
 
-
-
+# All functions to use to generate the stats
 
 def average_followers(dataset):
     """Function to calculate the average number of followers"""
     user_count = len(dataset)
     followers_count = 0
+
     for data in dataset:
         followers_count += data[3]
+
     average = followers_count / user_count
+
     return average
 
 
 def average_followed(dataset):
     """Function to calculate the average number of friends (followed)"""
+
     user_count = len(dataset)
     followed_count = 0
+
     for data in dataset:
         followed_count += data[4]
+
     average = followed_count / user_count
+
     return average
 
 
 def average_status_count(dataset):
-    """Function to find the average number of statuses"""
+    """Function to calculate the average number of statuses (tweets written)"""
+
     user_count = len(dataset)
     statuses_count = 0
+
     for data in dataset:
         statuses_count += data[2]
+
     average = statuses_count / user_count
+
     return average
 
 
 def find_most_statuses(dataset):
-    """Functions to find user with most tweets"""
+    """Functions to find user with most written tweets"""
+
     highest_statuses = 0
     user_name = ""
+
     for data in dataset:
         if data[2] > highest_statuses:
             highest_statuses = data[2]
             user_name = data[1]
+
     return highest_statuses, user_name
 
 
 def find_least_statuses(dataset):
-    """Functions to find user with least tweets"""
+    """Functions to find user with least writtentweets"""
+
     least_statuses = dataset[0][2]
     user_name = ""
+
     for data in dataset:
         if data[2] < least_statuses:
             least_statuses = data[2]
             user_name = data[1]
+
     return least_statuses, user_name
 
 
 def find_highest_followers(dataset):
     """Functions to find user with most followers"""
+
     highest_followers = 0
     user_name = ""
+
     for data in dataset:
         if data[3] > highest_followers:
             highest_followers = data[3]
             user_name = data[1]
+
     return highest_followers, user_name
 
 
 def find_highest_friends(dataset):
-    """Functions to find user with most friends"""
+    """Functions to find user with most friends (followed accounts)"""
+
     highest_friends = 0
     user_name = ""
+
     for data in dataset:
         if data[4] > highest_friends:
             highest_friends = data[4]
             user_name = data[1]
+
     return highest_friends, user_name
 
 
 def find_users_without_followers(dataset, limit=5):
-    """Functions to find users without followers"""
+    """Functions to find x users without followers"""
+
     users_without_followers = []
+
     for data in dataset:
         if data[3] == 0:
             users_without_followers.append(data[1])
@@ -99,8 +122,10 @@ def find_users_without_followers(dataset, limit=5):
 
 
 def find_users_most_followers(dataset, limit=5):
-    """Functions to find users with the biggest number of followers"""
+    """Functions to find x users with the biggest number of followers"""
+
     users_with_followers = []
+
     for data in dataset:
         user_with_count = (data[3], data[1])
         users_with_followers.append(user_with_count)
@@ -111,7 +136,9 @@ def find_users_most_followers(dataset, limit=5):
         limit = len(users_with_followers)
 
     users_with_followers = users_with_followers[-limit:]
+
     result_list = []
+
     for user in users_with_followers:
         result_list.append(f"{user[1]} ({user[0]})")
 
@@ -120,8 +147,10 @@ def find_users_most_followers(dataset, limit=5):
 
 def find_smallest_highest_id(dataset):
     """Function to find the smallest and highest ID"""
+
     smallest_id = [dataset[0][1], int(dataset[0][0])]
     biggest_id = [dataset[0][1], 0]
+
     for data in dataset:
         if int(data[0]) < smallest_id[1]:
             smallest_id[0] = data[1]
@@ -135,8 +164,10 @@ def find_smallest_highest_id(dataset):
 
 
 def find_users_without_followers_but_statuses(dataset, limit_followers=50, limit_statuses=500):
-    """Functions to find users without followers"""
+    """Functions to find users with x statuses, but y followers"""
+
     users_without_followers = []
+
     for data in dataset:
         if data[3] <= limit_followers and data[2] >= limit_statuses:
             users_without_followers.append(data[1])
@@ -146,31 +177,39 @@ def find_users_without_followers_but_statuses(dataset, limit_followers=50, limit
 
 def percentage_user_with_less_x_followers(dataset, limit=100):
     """Function to find the percentage of users with less than x followers"""
+
     users_with_less = 0
+
     for data in dataset:
         if data[3] <= limit:
             users_with_less += 1
 
     percentage = (users_with_less * 100)/len(dataset)
+
     return percentage, limit
 
 
 def average_followers_users_no_friends(dataset, limit=5):
-    """Function to determine the average of followers of users with less than limit friends"""
+    """Function to determine the average follower count of users with less than x friends"""
+
     followers_users_no_friends = []
+
     for data in dataset:
         if data[4] <= limit:
             followers_users_no_friends.append(data[3])
 
     average = sum(followers_users_no_friends) / len(followers_users_no_friends)
+
     return average, limit
 
 
 def find_highest_followers_friends_ratio(dataset):
-    """Function to find the user with the highest follwers/friends ration"""
+    """Function to find the user with the highest followers/friends ratio"""
+
     username_highest_ratio = ()
     highest_ratio = 0
     followers_friends = (0, 0)
+
     for data in dataset:
         if data[4] != 0 and data[3] / data[4] > highest_ratio:
             highest_ratio = data[3] / data[4]
@@ -181,11 +220,14 @@ def find_highest_followers_friends_ratio(dataset):
 
 
 def find_average_followers_friends_ratio(dataset):
-    """Function to find the user with the highest follwers/friends ration"""
+    """Function to find the average followers/friends ratio"""
+
     followers_friends_ratios = []
+
     for data in dataset:
         if data[4] != 0:
             followers_friends_ratios.append(data[3] / data[4])
 
     average = sum(followers_friends_ratios) / len(followers_friends_ratios)
+
     return average
