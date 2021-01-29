c34c43fb5fb332488a5f02890556d5f9169807cf
nicolasgasco <nicolas.gasco92@gmail.com>
2021-01-29 14:39:36 +0100

    Created new file for authenticating to Twitter API

---
+++ scripts/twitter_authentication.py
@@ -0,0 +1,15 @@
+import os
+import tweepy
+
+# This file handles the connection to the Twitter API and avoids repetition of code in other files
+
+# Fetch the secrets and keys/tokens from virtual environment variables
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
